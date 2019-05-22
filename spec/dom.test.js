aff083850a959cc67b01ddd3908c21f813dc9a09
Erzhena Shirabon <erzhena.shirabon@gmail.com>
2019-05-21 21:41:36 -0700

    Add one react component test

--- spec/dom.test.js
+++ spec/dom.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import ListEntry from '../client/src/components/ListEntry.jsx';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
+import ListEntry from '../client/src/components/ListEntry';
+configure({ adapter: new Adapter() });
 
 test('component of ListEntry', () => {
-  const listEntry = shallow(<ListEntry/>)
+  var obj = {
+    id: 1,
+    img: 'path.jpg',
+    house_type: 'house',
+    location: 'Berlin',
+    description: 'sunny',
+    cost_per_night: 250,
+    rating: 1.2,
+    votes: 4
+  };
+  const entry = shallow(<ListEntry entry={obj} key={obj.id} />);
+  expect(entry.text()).not.toBeNull();
 });
